9eb2d6315a7107836708003af60baf5787d09c68
gibs0nnn <gibs0n@knights.ucf.edu>
2021-08-31 03:38:04 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solution
+ *  Copyright 2021 Nathaniel Gibson
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = scanner.nextLine();
+        System.out.print("Who said it? ");
+        String name = scanner.nextLine();
+        System.out.println(name + " says, " + "\"" + quote + "\"");
+    }
+}
